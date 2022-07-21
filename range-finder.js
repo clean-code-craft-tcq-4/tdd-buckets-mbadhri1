57fb28b506577ce4f4c29cdf65ce64bcdce5d381
Badhrinarayanan <badhrinarayanan.m@siemens.com>
2022-07-21 16:48:10 +0530

    lint issue fixed

--- range-finder.js
+++ range-finder.js
@@ -1,51 +1,48 @@
 function sort(array) {
-	return array.sort(function (a, b) {
-		return a - b;
-	});
+  return array.sort(function(a, b) {
+    return a - b;
+  });
 }
 
 function findRanges(sortedInput) {
-	let minContinous = 0;
-	let maxContinous;
-	let continousCount = 1;
-	let ranges = [];
-	sortedInput.forEach((element, index) => {
-		const difference = sortedInput[index + 1] - sortedInput[index];
-		if ([0, 1].includes(difference)) {
-			maxContinous = index + 1;
-			continousCount++;
-		} else {
-			if (continousCount > 1) {
-				ranges.push({
-					min: sortedInput[minContinous],
-					max: sortedInput[maxContinous],
-					continousCount,
-				});
-				maxContinous = undefined;
-				continousCount = 1;
-			}
-			minContinous = index + 1;
-		}
-	});
-	return ranges;
+  let minContinous = 0;
+  let maxContinous;
+  let continousCount = 1;
+  const ranges = [];
+  sortedInput.forEach((element, index) => {
+    const difference = sortedInput[index + 1] - sortedInput[index];
+    if ([0, 1].includes(difference)) {
+      maxContinous = index + 1;
+      continousCount++;
+    } else {
+      if (continousCount > 1) {
+        ranges.push({
+          min: sortedInput[minContinous],
+          max: sortedInput[maxContinous],
+          continousCount,
+        });
+        maxContinous = undefined;
+        continousCount = 1;
+      }
+      minContinous = index + 1;
+    }
+  });
+  return ranges;
 }
 function printHeader() {
-	console.log('Range, Readings');
+  console.log('Range, Readings');
 }
 
-// function rangeFormatter(range) {
-// 	console.log(`${range.min}-${range.max} ${range.continousCount}`);
-// }
 
 function rangeFormatter(ranges) {
-	printHeader();
-	ranges.forEach((range) => {
-		console.log(`${range.min}-${range.max} ${range.continousCount}`);
-	});
+  printHeader();
+  ranges.forEach((range) => {
+    console.log(`${range.min}-${range.max} ${range.continousCount}`);
+  });
 }
 
 module.exports = {
-	sort,
-	findRanges,
+  sort,
+  findRanges,
 };
-// rangeFormatter(findRanges(sort([3, 3, 5, 7, 8, 9, 11, 12, 13])));
+rangeFormatter(findRanges(sort([3, 3, 5, 7, 8, 9, 11, 12, 13])));
--- test/range-test.js
+++ test/range-test.js
@@ -1,71 +1,71 @@
-const { expect } = require('chai');
-const { findRanges, sort } = require('../range-finder');
+const {expect} = require('chai');
+const {findRanges, sort} = require('../range-finder');
 
 it('should sort the values', () => {
-	const input = [4, 2, 3, 1];
-	expect(sort(input)).to.deep.equal([1, 2, 3, 4]);
+  const input = [4, 2, 3, 1];
+  expect(sort(input)).to.deep.equal([1, 2, 3, 4]);
 });
 
 it(' for empty array', () => {
-	const input = [];
-	expect(findRanges(sort(input))).to.deep.equal([]);
+  const input = [];
+  expect(findRanges(sort(input))).to.deep.equal([]);
 });
 
 it(' for  array with 1 element', () => {
-	const input = [1];
-	expect(findRanges(sort(input))).to.deep.equal([]);
+  const input = [1];
+  expect(findRanges(sort(input))).to.deep.equal([]);
 });
 
 it(' for  array with 2 consecutive element', () => {
-	const input = [1, 2];
-	expect(findRanges(sort(input))).to.deep.equal([
-		{
-			continousCount: 2,
-			max: 2,
-			min: 1,
-		},
-	]);
+  const input = [1, 2];
+  expect(findRanges(sort(input))).to.deep.equal([
+    {
+      continousCount: 2,
+      max: 2,
+      min: 1,
+    },
+  ]);
 });
 
 it(' for  array with 2 same element', () => {
-	const input = [2, 2];
-	expect(findRanges(sort(input))).to.deep.equal([
-		{
-			continousCount: 2,
-			max: 2,
-			min: 2,
-		},
-	]);
+  const input = [2, 2];
+  expect(findRanges(sort(input))).to.deep.equal([
+    {
+      continousCount: 2,
+      max: 2,
+      min: 2,
+    },
+  ]);
 });
 
 it(' for  array with n same element', () => {
-	const input = [2, 2, 2, 2];
-	expect(findRanges(sort(input))).to.deep.equal([
-		{
-			continousCount: 4,
-			max: 2,
-			min: 2,
-		},
-	]);
+  const input = [2, 2, 2, 2];
+  expect(findRanges(sort(input))).to.deep.equal([
+    {
+      continousCount: 4,
+      max: 2,
+      min: 2,
+    },
+  ]);
 });
 
 it(' for  array with n different non continous element', () => {
-	const input = [1, 3, 5, 7];
-	expect(findRanges(sort(input)), input).to.deep.equal([]);
+  const input = [1, 3, 5, 7];
+  expect(findRanges(sort(input)), input).to.deep.equal([]);
 });
 
 it(' for  array with n different continous and non continous element', () => {
-	const input = [3, 3, 5, 4, 10, 11, 12];
-	expect(findRanges(sort(input))).to.deep.equal([
-		{
-			continousCount: 4,
-			max: 5,
-			min: 3,
-		},
-		{
-			continousCount: 3,
-			max: 12,
-			min: 10,
-		},
-	]);
+  const input = [3, 3, 5, 4, 10, 11, 12];
+  expect(findRanges(sort(input))).to.deep.equal([
+    {
+      continousCount: 4,
+      max: 5,
+      min: 3,
+    },
+    {
+      continousCount: 3,
+      max: 12,
+      min: 10,
+    },
+  ]);
 });
